f1079c8d12d4f8349d0b140a4a540638e49f4493
Pavel <pvlplv@pm.me>
2021-02-07 23:55:08 +0300

    new progress

--- packages/web/src/components/player/PlayerProgressBar.svelte
+++ packages/web/src/components/player/PlayerProgressBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import TimeString from "./TimeString.svelte"
+  import Progress from "./Progress.svelte"
 
   export let currentTime = 0
   export let duration = 0
@@ -10,14 +11,7 @@
 
 <div>
   <div class="time"><TimeString time={currentTime} {showHours} /></div>
-  <input
-    type="range"
-    bind:value={currentTime}
-    on:input
-    on:change
-    max={duration * 0.99999}
-    step={duration / 10000}
-  />
+  <Progress {currentTime} {duration} on:input on:change />
   <div class="time"><TimeString time={timeLeft} {showHours} /></div>
 </div>
 
@@ -27,59 +21,11 @@
     justify-content: center;
     align-items: center;
     margin: 0;
-    margin-top: 1rem;
-  }
-
-  /*Chrome*/
-  @media screen and (-webkit-min-device-pixel-ratio: 0) {
-    input[type="range"] {
-      overflow: hidden;
-      width: 13.8rem;
-      margin-left: 1rem;
-      margin-right: 1rem;
-      height: 0.2rem;
-      -webkit-appearance: none;
-      background-color: #c4c4c4;
-      border-color: transparent;
-      border: 0px solid #000101;
-      color: transparent;
-    }
-
-    input[type="range"]::-webkit-slider-runnable-track {
-      height: 0px;
-      -webkit-appearance: none;
-      color: #000000;
-      margin-top: -1px;
-    }
-
-    input[type="range"]::-webkit-slider-thumb {
-      width: 0.01rem;
-      height: 0.01rem;
-      -webkit-appearance: none;
-      cursor: pointer;
-      background: #000a64;
-      box-shadow: -120px 0 0 120px #001aff;
-    }
-  }
-
-  /** FF*/
-  input[type="range"]::-moz-range-progress {
-    background-color: #001aff;
-  }
-  input[type="range"]::-moz-range-track {
-    background-color: #c4c4c4;
-  }
-  /* IE*/
-  input[type="range"]::-ms-fill-lower {
-    background-color: #001aff;
-  }
-  input[type="range"]::-ms-fill-upper {
-    background-color: #c4c4c4;
   }
 
   .time {
     font-family: HelveticaNeue, Helvetica, sans-serif;
-    margin: 0rem;
+    margin: 0rem 0.5rem;
     color: rgba(0, 0, 0, 0.8);
     line-height: 150%;
     font-size: 0.9rem;
---
+++ packages/web/src/components/player/Progress.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte"
+
+  export let currentTime = 0
+  export let duration = 0
+
+  let div: HTMLDivElement
+  let width: number
+  let seeking = false
+  let newTime = 0
+
+  $: time = seeking ? newTime : currentTime
+  $: position = (time / duration) * 100
+  $: rect = div?.getBoundingClientRect()
+  $: left = rect?.left
+  $: width = rect?.width
+
+  const dispatch = createEventDispatcher()
+
+  function hadlePointerMove(event) {
+    const pointerPositionX = event.clientX - left
+    newTime = (pointerPositionX / width) * duration
+    if (newTime > duration) {
+      newTime = 0.99 * duration
+    } else if (newTime < 0) {
+      newTime = 0
+    }
+    if (seeking) {
+      dispatch("input", { newTime })
+    }
+  }
+
+  function handlePointerUp(event) {
+    if (seeking) {
+      dispatch("change", { newTime })
+      seeking = false
+    }
+  }
+
+  function handlePointerDown(event) {
+    event.preventDefault()
+    seeking = true
+  }
+</script>
+
+<svelte:window
+  on:pointermove={hadlePointerMove}
+  on:pointerup={handlePointerUp}
+/>
+
+<div
+  role="progressbar"
+  aria-valuenow={position}
+  aria-valuemin={0}
+  aria-valuemax={duration}
+  aria-valuetext=""
+  bind:this={div}
+  on:pointerdown={handlePointerDown}
+>
+  <div class="background" />
+  <div class="progress" style="width: {position}%" />
+  <div class="thumb" style="left: {position}%" />
+</div>
+
+<style>
+  div {
+    position: relative;
+    width: 100%;
+    height: 2px;
+    margin: 1rem 0;
+    padding: 0;
+    touch-action: none;
+  }
+
+  div.background {
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    margin-top: -1px;
+    padding: 0;
+    border-radius: 1px;
+    background-color: gray;
+  }
+
+  div.thumb {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    border-radius: 100%;
+    margin-top: -4px;
+    margin-left: -4px;
+    padding: 0;
+    background-color: black;
+  }
+
+  div.progress {
+    position: absolute;
+    height: 2px;
+    margin-top: -1px;
+    padding: 0;
+    border-radius: 1px;
+    background-color: darkblue;
+  }
+</style>
